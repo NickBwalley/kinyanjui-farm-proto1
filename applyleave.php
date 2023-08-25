6318fcaac6256b2e9386709aaba3bb5b9805afba
NickBwalley <nickbwalley@gmail.com>
2023-08-25 23:46:37 +0300

    third commit

--- applyleave.php
+++ applyleave.php
@@ -10,14 +10,14 @@
 
 <html>
 <head>
-	<title>Apply Leave | Employee Panel | XYZ Corporation</title>
+	<title>Apply Leave | Employee Panel </title>
 	<link rel="stylesheet" type="text/css" href="styleapply.css">
 </head>
 <body bgcolor="#F0FFFF">
 	
 	<header>
 		<nav>
-			<h1>XYZ Corp.</h1>
+			<h1>Kinyanjui Farm.</h1>
 			<ul id="navli">
 				<li><a class="homeblack" href="eloginwel.php?id=<?php echo $id?>"">HOME</a></li>
 				<li><a class="homeblack" href="myprofile.php?id=<?php echo $id?>"">My Profile</a></li>
