1470617abf96891ac73b93f7d0749047ee6eaf3a
NickBwalley <nickbwalley@gmail.com>
2023-09-02 00:30:23 +0300

    lastcommit

--- eloginwel.php
+++ eloginwel.php
@@ -1,3 +1,11 @@
+<?php
+// Prevent caching
+header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+header("Cache-Control: post-check=0, pre-check=0", false);
+header("Pragma: no-cache");
+?>
+
+
 <?php 
 	session_start();
 	require_once ('process/dbh.php');
