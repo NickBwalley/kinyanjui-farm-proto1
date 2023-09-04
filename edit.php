62b46a884b2a0664b624489e8c32b4a152674839
NickBwalley <nickbwalley@gmail.com>
2023-09-04 23:47:57 +0300

    aprecommit

--- edit.php
+++ edit.php
@@ -98,7 +98,7 @@ $result = mysqli_query($conn, "UPDATE `manager` SET `firstName`='$firstname',`la
 		<nav>
 			<h1>XYZ Corp.</h1>
 			<ul id="navli">
-				<li><a class="homeblack" href="index.html">HOME</a></li>
+				<li><a class="homeblack" href="aloginwel.php?id=<?php echo $id ?>">HOME</a></li>
 				<li><a class="homeblack" href="addemp.php">Add Employee</a></li>
 				<li><a class="homered" href="viewemp.php">View Employee</a></li>
 				<li><a class="homeblack" href="elogin.html">Log Out</a></li>
