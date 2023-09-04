deef2a802d3bd0ec610a42fe2864ace4ff949c3c
NickBwalley <nickbwalley@gmail.com>
2023-09-04 10:00:51 +0300

    precommit

--- epayments.php
+++ epayments.php
@@ -153,7 +153,10 @@ $result = mysqli_query($conn, "UPDATE `employee` SET `firstName`='$firstname',`l
 
                         <input type="hidden" name="id" id="textField" value="<?php echo $id;?>" required="required"><br><br>
                         <div class="p-t-20">
-                            <button class="btn btn--radius btn--green" type="submit" name="update">Approve</button>
+                            <button class="btn btn--radius btn--green" type="submit" name="update">
+  <a href="msalaryemp.php" style="text-decoration: none; color: inherit; display: block; width: 100%; height: 100%;">Approve</a>
+</button>
+
 							<button class="btn btn--radius btn--red" type="submit" name="update">Cancel</button>
                         </div>
                         
