70e38a2fd6d3385d8bac563a0e33f63fb71a9a04
NickBwalley <nickbwalley@gmail.com>
2023-09-04 09:10:08 +0300

    precommit

--- epayments.php
+++ epayments.php
@@ -1,91 +1,209 @@
 <?php
+session_start();
+require_once('process/dbh.php'); // Make sure this file includes your database connection ($conn).
 
-require_once ('process/dbh.php');
-$sql = "SELECT employee.id,employee.firstName,employee.lastName,salary.base,salary.bonus,salary.total from employee,`salary` where employee.id=salary.id";
+// Fetch employees and their ranks using JOIN
+$sql = "SELECT * FROM `employee` e
+        JOIN `rank` r ON e.id = r.eid";
 
-//echo "$sql";
 $result = mysqli_query($conn, $sql);
 
-?>
-<?php 
-require_once('process/dbh.php');
 
 $id = isset($_GET['id']) ? mysqli_real_escape_string($conn, $_GET['id']) : '';
 
 if (!empty($id)) {
-    $managerSql = "SELECT * FROM `manager` WHERE id = '$id'";
-    $managerResult = mysqli_query($conn, $managerSql);
+    $employeeSql = "SELECT * FROM `employee` WHERE id = '$id'";
+    $employeeResult = mysqli_query($conn, $employeeSql);
 
-    if (!$managerResult) {
-        die("Error fetching manager: " . mysqli_error($conn));
+    if (!$employeeResult) {
+        die("Error fetching employee: " . mysqli_error($conn));
     }
 
-    $manager = mysqli_fetch_array($managerResult);
+    $manager = mysqli_fetch_array($employeeResult);
     $empName = $manager['firstName'];
+}
+
+// Check if the session variable 'userID' is set
+if (isset($_SESSION['manID'])) {
+    // Access the userID from the session
+    $userID = $_SESSION['manID'];
+
+    // Now you can use $userID in your code
+    echo "User ID: $userID";
+} else {
+    // Handle the case where the session variable is not set
+    echo "User ID not found in session.";
+}
+
+$sql = "SELECT * FROM `employee` WHERE 1";
+$sql1 = "SELECT * FROM `salary` WHERE id = $id";
+
+//echo "$sql";
+$result = mysqli_query($conn, $sql);
+$result1 = mysqli_query($conn, $sql);
+if(isset($_POST['update']))
+{
+
+	$id = mysqli_real_escape_string($conn, $_POST['id']);
+	$firstname = mysqli_real_escape_string($conn, $_POST['firstName']);
+	$lastname = mysqli_real_escape_string($conn, $_POST['lastName']);
+	$email = mysqli_real_escape_string($conn, $_POST['email']);
+	$birthday = mysqli_real_escape_string($conn, $_POST['birthday']);
+	$contact = mysqli_real_escape_string($conn, $_POST['contact']);
+	$address = mysqli_real_escape_string($conn, $_POST['address']);
+	$gender = mysqli_real_escape_string($conn, $_POST['gender']);
+	$dept = mysqli_real_escape_string($conn, $_POST['dept']);
+	//$salary = mysqli_real_escape_string($conn, $_POST['salary']);
+
+
+
+
+
+	// $result = mysqli_query($conn, "UPDATE `employee` SET `firstName`='$firstname',`lastName`='$lastname',`email`='$email',`password`='$email',`gender`='$gender',`contact`='$contact',`nid`='$nid',`salary`='$salary',`address`='$address',`dept`='$dept',`degree`='$degree' WHERE id=$id");
+
+
+$result = mysqli_query($conn, "UPDATE `employee` SET `firstName`='$firstname',`lastName`='$lastname',`email`='$email',`birthday`='$birthday',`gender`='$gender',`contact`='$contact',`address`='$address',`dept`='$dept' WHERE id=$id");
+	echo ("<SCRIPT LANGUAGE='JavaScript'>
+    window.alert('Succesfully Updated')
+    </SCRIPT>");
+
+    header("Location: mviewemployee.php?id=$userID");
+
+
+	
 }
 ?>
 
 
 
+
+<?php
+	$id = (isset($_GET['id']) ? $_GET['id'] : '');
+	$sql = "SELECT * from `employee` WHERE id=$id";
+	$result = mysqli_query($conn, $sql);
+	if($result){
+	while($res = mysqli_fetch_assoc($result)){
+	$firstname = $res['firstName'];
+	$lastname = $res['lastName'];
+	$email = $res['email'];
+	$contact = $res['contact'];
+	$address = $res['address'];
+	$gender = $res['gender'];
+	$birthday = $res['birthday'];
+	$dept = $res['dept'];
+	
+}
+}
+
+?>
+
 <html>
 <head>
-	<title>Payments | Kinyanjui Farm</title>
-	<link rel="stylesheet" type="text/css" href="styleview.css">
+	<title>View Employee |  Admin Panel </title>
+	<!-- Icons font CSS-->
+    <link href="vendor/mdi-font/css/material-design-iconic-font.min.css" rel="stylesheet" media="all">
+    <link href="vendor/font-awesome-4.7/css/font-awesome.min.css" rel="stylesheet" media="all">
+    <!-- Font special for pages-->
+    <link href="https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i" rel="stylesheet">
+
+    <!-- Vendor CSS-->
+    <link href="vendor/select2/select2.min.css" rel="stylesheet" media="all">
+    <link href="vendor/datepicker/daterangepicker.css" rel="stylesheet" media="all">
+
+    <!-- Main CSS-->
+    <link href="css/main.css" rel="stylesheet" media="all">
 </head>
 <body>
-	
 	<header>
 		<nav>
 			<h1>Kinyanjui Farm.</h1>
 			<ul id="navli">
-	            <li><a class="homeblack" href="eloginwel.php?id=<?php echo $id?>"">HOME</a></li>
-                <li><a class="homeblack" href="maddemp.php?id=<?php echo $id?>"">Add Employee</a></li>
-                <li><a class="homeblack" href="mviewemployee.php?id=<?php echo $id?>"">View Employee</a></li>
-                <li><a class="homeblack" href="massign.php?id=<?php echo $id?>"">Assign Project</a></li>
-                <li><a class="homeblack" href="massignproject.php?id=<?php echo $id?>"">Project Status</a></li>
-                <li><a class="homeblack" href="msalaryemp.php?id=<?php echo $id?>"">Salary Table</a></li> 
-                <li><a class="homeblack" href="mempleave.php?id=<?php echo $id?>"">Employee Leave</a></li>
-                <li><a class="homeblack" href="mapplyleave.php?id=<?php echo $id?>"">Apply Leave</a></li>
+				<li><a class="homeblack" href="eloginwel.php?id=<?php echo $userID?>"">HOME</a></li>
+				<li><a class="homered" href="mviewemployee.php?id=<?php echo $id?>"">View Employee</a></li>
 				<li><a class="homeblack" href="elogin.html">Log Out</a></li>
 			</ul>
 		</nav>
 	</header>
-	 
-	<div class="divider"></div>
-	<div id="divimg">
-		
-	</div>
 	
-	<table>
-			<tr>
-				<th align = "center">Emp. ID</th>
-				<th align = "center">Name</th>
-				
-				
-				<th align = "center">Price per KG</th>
-				<th align = "center">KGS harvested</th>
-				<th align = "center">TotalPayment</th>
-				
-				
-			</tr>
-			
-			<?php
-				while ($employee = mysqli_fetch_assoc($result)) {
-					echo "<tr>";
-					echo "<td>".$employee['id']."</td>";
-					echo "<td>".$employee['firstName']." ".$employee['lastName']."</td>";
-					
-					echo "<td>".$employee['base']."</td>";
-					echo "<td>".$employee['bonus']." %</td>";
-					echo "<td>".$employee['total']."</td>";
-					
-					
+	<div class="divider"></div>
+	
 
-				}
+		<!-- <form id = "registration" action="edit.php" method="POST"> -->
+	<div class="page-wrapper bg-blue p-t-100 p-b-100 font-robo">
+        <div class="wrapper wrapper--w680">
+            <div class="card card-1">
+                <div class="card-heading"></div>
+                <div class="card-body">
+                    <h2 class="title">Pay <?php echo $firstname .' '. $lastname?> </h2>
+                    <form id = "registration" action="edit2.php?id=<?php echo $userID?>"" method="POST">
+
+                        <div class="row row-space">
+                            <div class="col-2">
+                                <div class="input-group">
+                                     <input class="input--style-1" type="text" name="firstName" value="<?php echo $firstname;?>" >
+                                </div>
+                            </div>
+                            <div class="col-2">
+                                <div class="input-group">
+                                    <input class="input--style-1" type="text" name="lastName" value="<?php echo $lastname;?>">
+                                </div>
+                            </div>
+                        </div>
 
 
-			?>
-			
-			</table>
+
+
+
+                        <div class="input-group">
+                            <input class="input--style-1" type="email"  name="email" value="<?php echo $email;?>">
+                        </div>
+                        <div class="row row-space">
+                            <div class="col-2">
+                                <div class="input-group">
+                                    <input class="input--style-1" type="text" name="birthday" value="<?php echo $birthday;?>">
+                                   
+                                </div>
+                            </div>
+                            <div class="col-2">
+                                <div class="input-group">
+									<input class="input--style-1" type="text" name="gender" value="<?php echo $gender;?>">
+                                </div>
+                            </div>
+                        </div>
+                        
+                        <div class="input-group">
+                            <input class="input--style-1" type="number" name="contact" value="<?php echo $contact;?>">
+                        </div>
+
+                       
+                         <div class="input-group">
+                            <input class="input--style-1" type="text"  name="address" value="<?php echo $address;?>">
+                        </div>
+
+                        <div class="input-group">
+                            <input class="input--style-1" type="text" name="dept" value="<?php echo $dept;?>">
+                        </div>
+
+                        <input type="hidden" name="id" id="textField" value="<?php echo $id;?>" required="required"><br><br>
+                        <div class="p-t-20">
+                            <button class="btn btn--radius btn--green" type="submit" name="update">Submit</button>
+                        </div>
+                        
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+     <!-- Jquery JS-->
+    <!-- <script src="vendor/jquery/jquery.min.js"></script>
+   
+    <script src="vendor/select2/select2.min.js"></script>
+    <script src="vendor/datepicker/moment.min.js"></script>
+    <script src="vendor/datepicker/daterangepicker.js"></script>
+
+   
+    <script src="js/global.js"></script> -->
 </body>
 </html>
