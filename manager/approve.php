33396ca8fe57a3181fcf78fa8f432b971cede47c
NickBwalley <nickbwalley@gmail.com>
2023-09-19 09:35:14 +0300

    approve verified

--- manager/approve.php
+++ manager/approve.php
@@ -17,10 +17,10 @@ if (isset($_SESSION['manID'])) {
 
 //getting id of the data from url
 $id = $_GET['id'];
-$token = $_GET['token'];
+// $token = $_GET['token'];
 
 //deleting the row from table
-$result = mysqli_query($conn, "UPDATE `employee_leave` SET `status`='Approved' WHERE id = $id AND token = $token;");
+$result = mysqli_query($conn, "UPDATE `employee_leave` SET `status`='Approved' WHERE id = $id ");
 
 //redirecting to the display page (index.php in our case)
 header("Location: managerEmployeeLeave.php?id=$userID");
