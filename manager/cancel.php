2e73e36f563bc86fc3c42c1888491354e81e42f1
NickBwalley <nickbwalley@gmail.com>
2023-09-19 09:35:59 +0300

    aprecommit

--- manager/cancel.php
+++ manager/cancel.php
@@ -17,9 +17,9 @@ if (isset($_SESSION['manID'])) {
 
 //getting id of the data from url
 $id = $_GET['id'];
-$token = $_GET['token'];
+// $token = $_GET['token'];
 //deleting the row from table
-$result = mysqli_query($conn, "UPDATE `employee_leave` SET `status`='Cancelled' WHERE `id`=$id and `token` = $token");
+$result = mysqli_query($conn, "UPDATE `employee_leave` SET `status`='Cancelled' WHERE `id`=$id ");
 
 //redirecting to the display page (index.php in our case)
 header("Location: managerEmployeeLeave.php?id=$userID");
