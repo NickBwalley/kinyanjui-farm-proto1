97b682158ca3c2f0c6103571fcf89199b304ea56
NickBwalley <nickbwalley@gmail.com>
2023-09-19 22:36:39 +0300

    adminHome

--- Admin/adminHome.php
+++ Admin/adminHome.php
@@ -18,12 +18,12 @@ header("Pragma: no-cache");
 	 $employeen = mysqli_fetch_array($result1);
 	//  $empName = ($employeen['firstName']);
 
-	$sql = "SELECT id, firstName, lastName,  points FROM employee, rank WHERE rank.eid = employee.id order by rank.points desc";
+	$sql = "SELECT id, firstName, lastName,  total_kgs_harvested FROM employee, employee_salary WHERE employee_salary.eid = employee.id order by employee_salary.total_kgs_harvested desc";
 	$sql1 = "SELECT `pname`, `duedate` FROM `project` WHERE eid = $id and status = 'Due'";
 
-	$sql2 = "Select * From employee, employee_leave Where employee.id = $id and employee_leave.id = $id order by employee_leave.token";
+	$sql2 = "Select * From employee, employee_leave Where employee.id = $id and employee_leave.id = $id order by employee_leave.id";
 
-	$sql3 = "SELECT * FROM `salary` WHERE id = $id";
+	$sql3 = "SELECT * FROM `employee_salary_base` WHERE id = $id";
 
 //echo "$sql";
 $result = mysqli_query($conn, $sql);
@@ -102,11 +102,11 @@ $result4 = mysqli_query($conn, $sql4);
 					
 					echo "<td>".$employee['firstName']." ".$employee['lastName']."</td>";
 					
-					echo "<td>".$employee['points']."</td>";
+					echo "<td>".$employee['total_kgs_harvested']."</td>";
 
-					// Multiply $employee['points'] by 8 and display the result
-					$pointsMultiplied = $employee['points'] * 8;
-					echo "<td>" . $pointsMultiplied . "</td>";
+					// Multiply $employee['total_kgs_harvested'] by 8 and display the result
+					$total_kgs_harvestedMultiplied = $employee['total_kgs_harvested'] * 8;
+					echo "<td>" . $total_kgs_harvestedMultiplied . "</td>";
 					echo "<td><a href=\"adminPayment.php?id=$employee[id]\">Pay</a></td>";
 					
 					$seq+=1;
@@ -117,122 +117,7 @@ $result4 = mysqli_query($conn, $sql4);
 
 		</table>
    
-    	<h2 style="font-family: 'Montserrat', sans-serif; font-size: 25px; text-align: center;">Due Projects</h2>
     	
-
-    	<table>
-
-			<tr>
-				<th align = "center">Task Details</th>
-				<th align = "center">Due Date</th>
-				
-			</tr>
-
-			
-
-			<?php
-				while ($employee1 = mysqli_fetch_assoc($result1)) {
-					echo "<tr>";
-					
-					echo "<td>".$employee1['pname']."</td>";
-					
-					echo "<td>".$employee1['duedate']."</td>";
-
-				}
-
-
-			?>
-
-		</table>
-
-
-
-		<h2 style="font-family: 'Montserrat', sans-serif; font-size: 25px; text-align: center;">Salary Status</h2>
-    	
-
-    	<table>
-
-			<tr>
-				
-				<th align = "center">Base Salary</th>
-				<th align = "center">Bonus</th>
-				<th align = "center">Total Salary</th>
-				
-			</tr>
-
-			
-
-			<?php
-				while ($employee = mysqli_fetch_assoc($result3)) {
-					
-
-					echo "<tr>";
-					
-					
-					echo "<td>".$employee['base']."</td>";
-					echo "<td>".$employee['bonus']." %</td>";
-					echo "<td>".$employee['total']."</td>";
-					
-				}
-
-
-				
-
-
-			?>
-
-		</table>
-
-
-
-
-
-
-
-
-
-
-		<h2 style="font-family: 'Montserrat', sans-serif; font-size: 25px; text-align: center;">Leave Status</h2>
-    	
-
-    	<table>
-
-			<tr>
-				
-				<th align = "center">Start Date</th>
-				<th align = "center">End Date</th>
-				<th align = "center">Total Days</th>
-				<th align = "center">Reason</th>
-				<th align = "center">Status</th>
-			</tr>
-
-			
-
-			<?php
-				while ($employee = mysqli_fetch_assoc($result2)) {
-					$date1 = new DateTime($employee['start']);
-					$date2 = new DateTime($employee['end']);
-					$interval = $date1->diff($date2);
-					$interval = $date1->diff($date2);
-
-					echo "<tr>";
-					
-					
-					echo "<td>".$employee['start']."</td>";
-					echo "<td>".$employee['end']."</td>";
-					echo "<td>".$interval->days."</td>";
-					echo "<td>".$employee['reason']."</td>";
-					echo "<td>".$employee['status']."</td>";
-					
-				}
-
-
-				
-
-
-			?>
-
-		</table>
 
 
 
